2c6c37a5f86c1d9d12838a90fcdb7504cb76c968
GeekTrainer <chrhar@microsoft.com>
2014-11-11 14:49:42 -0800

    Initial commit

---
+++ Work/BlankFlask/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+# If you get an error on the next line on Python 3.4.0, change to: Flask('app')
+# where app matches the name of this file without the .py extension.
+app = Flask(__name__)
+
+from routes import *
+from contextlib import closing
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app
+
+if __name__ == '__main__':
+    import os
+    host = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        port = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        port = 5555
+    app.run(host, port)
