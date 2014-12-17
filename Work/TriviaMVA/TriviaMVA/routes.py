68887b2753d177b8c179cf835ceb14e7b4bf10f8
GeekTrainer <charrison@geektrainer.com>
2014-12-17 12:07:37 -0800

    End of module 3

--- Work/TriviaMVA/TriviaMVA/routes.py
+++ Work/TriviaMVA/TriviaMVA/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for;
+from flask import Flask, url_for, request, render_template;
 from app import app;
 
 # server/
@@ -15,11 +15,44 @@ def hello():
                </html>""";
 
 # server/create
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    return "<h2>This is the create page!</h2>";
+    if request.method == 'GET':
+        # send the user the form
+        return render_template('CreateQuestion.html');
+    elif request.method == 'POST':
+        # read form data and save it
+        title = request.form['title'];
+        question = request.form['question'];
+        answer = request.form['answer'];
+
+        # Store data in data store
+        # Susan - please add code here
+        return render_template('CreatedQuestion.html', question = question);
+    else:
+        return "<h2>Invalid request</h2>";
 
 # server/question/<title>
-@app.route('/question/<title>')
+@app.route('/question/<title>', methods=['GET', 'POST'])
 def question(title):
-    return '<h2>' + title + '</h2>';
+    if request.method == 'GET':
+        # send the user the form
+
+        question = 'Question here.';
+        # Read question from data store
+        # Susan - please add code here
+        return render_template('AnswerQuestion.html', question = question);
+    elif request.method == 'POST':
+        # User has attempted answer. Check if they're correct
+        submittedAnswer = request.form['submittedAnswer'];
+
+        # Read answer from data store
+        # Susan - please add code here
+        answer = 'Answer';
+
+        if submittedAnswer == answer:
+            return render_template('Correct.html');
+        else:
+            return render_template('Incorrect.html', submittedAnswer = submittedAnswer, answer = answer);
+    else:
+        return '<h2>Invalid request</h2>';
